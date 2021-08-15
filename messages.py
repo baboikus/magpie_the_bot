55f53decd4e211f582dd1f4a52f1813aaf45d1d7
baboikus <varankinda@gmail.com>
2021-08-15 23:31:47 +0300

    add short descprtions for zero-args commands.

--- messages.py
+++ messages.py
@@ -1,26 +1,26 @@
 COMMANDS = [
-	("events", "", "show all important performers and tasks events."),
-	("daily_report", "", "show daily report describing probable risks and predictive analytics."),
-	("help", "", "show description for full list of commands."),
-	("backlog", "", "show current backlog."),
+	("events", "", "show important events", "show all important performers and tasks events."),
+	("daily_report", "", "show daily report", "show daily report describing probable risks and predictive analytics."),
+	("help", "", "show command description", "show full description for complete list of commands."),
+	("backlog", "", "show current backlog", "show current backlog."),
 
-	("task_add", "<task_id> [<tag1> ... <tagN>]", "add task <task_id> with specific tags <tag1>, ... , <tagN> to current backlog."),
-	("task_start", "<task_id>", "start new working session on the task <task_id>."),
-	("task_stop", "<task_id>", "stop current working session on the task <task_id>."),
+	("task_add", "<task_id> [<tag1> ... <tagN>]", "", "add task <task_id> with specific tags <tag1>, ... , <tagN> to current backlog."),
+	("task_start", "<task_id>", "", "start new working session on the task <task_id>."),
+	("task_stop", "<task_id>", "", "stop current working session on the task <task_id>."),
 
-	("tag_add", "<task_id> [<tag1> ... <tagN>]", "add set of tags <tag1>, ... , <tagN> to task <task_id>.")
+	("tag_add", "<task_id> [<tag1> ... <tagN>]", "", "add set of tags <tag1>, ... , <tagN> to task <task_id>.")
 ]
 
 def help_response():
 	response = "avaible commands:\n"
-	for command, args, description in COMMANDS:
-		response += "/% s % s - % s\n" % (command, args, description)
+	for command, args, _, full_description in COMMANDS:
+		response += "/% s % s - % s\n" % (command, args, full_description)
 	return response
 
 def botfather_help():
 	response = ""
-	for command, args, description in COMMANDS:
-		if args == "": response += "% s % s - % s\n" % (command, args, description)
+	for command, args, short_description, _ in COMMANDS:
+		if args == "": response += "% s % s - % s\n" % (command, args, short_description)
 	return response	
 
 
