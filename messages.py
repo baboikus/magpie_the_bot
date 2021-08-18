08311fe02253d98173ebb46e2215511bac9ba1cd
baboikus <varankinda@gmail.com>
2021-08-18 23:16:28 +0300

    add help descpription for /task_done command

--- messages.py
+++ messages.py
@@ -1,13 +1,13 @@
 COMMANDS = [
-	#TODO добавить описание для /task_done
 	("events", "", "show important events", "show all important performers and tasks events."),
 	("daily_report", "", "show daily report", "show daily report describing probable risks and predictive analytics."),
 	("help", "", "show command description", "show full description for complete list of commands."),
 	("backlog", "", "show current backlog", "show current backlog."),
 
 	("task_add", "<task_id> [<tag1> ... <tagN>]", "", "add task <task_id> with specific tags <tag1>, ... , <tagN> to current backlog."),
-	("task_start", "<task_id>", "", "start new working session on the task <task_id>."),
-	("task_stop", "<task_id>", "", "stop current working session on the task <task_id>."),
+	("task_start", "<task_id>", "", "start new working session on the task <task_id>. marking task in progress."),
+	("task_stop", "<task_id>", "", "stop current working session on the task <task_id>. suspend task if no one else working on task currently."),
+	("task_done", "<task_id>", "", "marking task <task_id> as done."),
 
 	("tag_add", "<task_id> [<tag1> ... <tagN>]", "", "add set of tags <tag1>, ... , <tagN> to task <task_id>.")
 ]
